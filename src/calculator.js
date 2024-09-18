7a47b00d72e129cbed6e5bb759cf157f626d88fd
royye6 <royye6@gmail.com>
2024-09-18 23:09:09 +0200

    calculator html

---
+++ src/calculator.js
@@ -0,0 +1,42 @@
+let displayScreenSection = document.getElementById('display');
+let resultScreenSection = document.getElementById('result');
+
+let clearButton = document.getElementById('clear');
+let hashButton = document.getElementById('hash');
+let percentButton = document.getElementById('percent');
+let divideButton = document.getElementById('divide');
+
+let sevenButton = document.getElementById('seven');
+let eightButton = document.getElementById('eight');
+let nineButton = document.getElementById('nine');
+let multiplyButton = document.getElementById('multiply');
+
+let fourButton = document.getElementById('four');
+let fiveButton = document.getElementById('five');
+let sixButton = document.getElementById('six');
+let addButton = document.getElementById('add');
+
+let oneButton = document.getElementById('one');
+let twoButton = document.getElementById('two');
+let threeButton = document.getElementById('three');
+
+let operation = [];
+
+clearButton = '0';
+percentButton = '%';
+divideButton = '/';
+sevenButton = '7';
+eightButton = '8';
+nineButton = '9';
+multiplyButton = '*';
+fourButton = '4';
+sixButton = '6';
+addButton = '+';
+oneButton = '1';
+twoButton = '2';
+threeButton = '3';
+
+percentButton.onclick 
+
+
+
